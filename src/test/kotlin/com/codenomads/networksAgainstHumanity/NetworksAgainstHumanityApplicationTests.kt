f37c68af29e3bc643725297580d39e97bf4e7d7d
jamsiedaly <jamsiedaly@gmail.com>
2020-01-17 11:46:29 +0100

    Initial commit

---
+++ src/test/kotlin/com/codenomads/networksAgainstHumanity/NetworksAgainstHumanityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codenomads.networksAgainstHumanity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NetworksAgainstHumanityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
